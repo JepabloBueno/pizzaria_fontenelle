eab3fe06622c6bc9446c3194b7f4795b57f0b7ea
Jean Pablo Bueno <jepablobueno@gmail.com>
2023-06-27 23:13:32 -0300

    Javascript

--- _scripts/scripts.js
+++ _scripts/scripts.js
@@ -74,12 +74,37 @@ $('ion-icon#twitter').click(function(){
 
 $('section#domingo-perfeito').waypoint(function(direcao){
     if (direcao == 'down') {
-    console.log('Passou por aqui!');
+        $('div.folha1').addClass('animate__animated animate__fadeInUp')
+        $('div.folha2').addClass('animate__animated animate__fadeInUp')
+        $('div.folha3').addClass('animate__animated animate__fadeInUp animate__slow')
+        $('div.camarao').addClass('animate__animated animate__fadeInUp animate__slow')
+        $('div.tomate').addClass('animate__animated animate__fadeInUp animate__slower')
+        $('div.queijo').addClass('animate__animated animate__fadeInUp animate__slower')
     }
 
     if (direcao == 'up') {
-        console.log('Agora subiu!')
+        $('div.folha1').removeClass('animate__animated animate__fadeOut')
+        $('div.folha2').removeClass('animate__animated animate__fadeOut')
+        $('div.folha3').removeClass('animate__animated animate__fadeOut')
+        $('div.camarao').removeClass('animate__animated animate__fadeOut')
+        $('div.tomate').removeClass('animate__animated animate__fadeOut')
+        $('div.queijo').removeClass('animate__animated animate__fadeOut')
     }
 }, {
-    offset: '100px;'
+    offset: '450px;'
 })
+
+$('section#pizza').waypoint(function(direcao){
+    if (direcao == 'down') {
+        $('section#pizza p').addClass('animate__animated animate__fadeInUp')
+        $('section#pizza div.pizza-opcoes').addClass('animate__animated animate__fadeInUp animate__slow')
+    }
+
+    if (direcao == 'up') {
+        $('section#pizza p').removeClassClass('animate__animated animate__fadeInUp')
+        $('section#pizza div.pizza-opcoes').removeClass('animate__animated animate__fadeInUp animate__slow')
+    }
+
+},{
+    offset:'450px;'
+}) 
